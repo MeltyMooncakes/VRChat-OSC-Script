aad1e1b831811aa47d8549c4c86da097119e0a0c
MeltyMooncakes <meltymooncakes@gmail.com>
2024-09-30 11:23:46 -0400

    added timeout function for future planned plugin support

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ export class Client {
 	
 	lastSent = 0;
 	enabled = false;
+
+	timeoutStart = 0;
+	timeoutLength = 0;
 	
 	music: Music;
 	config: ConfigData;
@@ -25,11 +28,16 @@ export class Client {
 		const lines = this.config.lines.map(data => new Line(data));
 
 		this.interval = setInterval(async () => {
+			if (this.timeoutLength > 0 && (Date.now() - this.timeoutStart) < this.timeoutLength) {
+				return;
+			}
+
 			for (const line of lines) {
 				line.getMessage();
 			}
 
 			if ((Date.now() - this.lastSent) > 1500) {
+				console.log(Date.now() - this.lastSent);
 				this.lastSent = Date.now();
 				this.send(`${await Line.formatMessage(this, lines.map(l => l.getMessage()).join("\n"))}`, true);
 			}
@@ -37,13 +45,21 @@ export class Client {
 	}
 	
 
+	/** Starts typing */
     sendTyping(bool = true) {
         this.socket.send(new Message("/chatbox/typing", bool));
     }
 
+	/** Sends a message */
     send(message: string, send = false) {
         this.socket.send(new Message("/chatbox/input", message, send));
     }
+
+	/** Times out the interval. */
+	timeout(time = 5e3) {
+		this.timeoutLength = time;
+		this.timeoutStart = Date.now();
+	}
 };
 
 const client = new Client();
