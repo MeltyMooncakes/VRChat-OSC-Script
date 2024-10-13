21921f2f4ded98608efa3a776b61e3d40093b72d
MeltyMooncakes <meltymooncakes@gmail.com>
2024-10-13 11:10:39 -0400

    testign

--- src/windows-music.ts
+++ src/windows-music.ts
@@ -2,5 +2,18 @@ import winplayer, { Status, Position } from "@innei/winplayer-rs/emitter";
 
 
 export async function getPlayer() {
-	return await winplayer();
+	const playerManager = await winplayer();
+	if (playerManager) {
+		playerManager.on("MediaPropertiesChanged", (status: Status) => {
+			console.log(status);
+		});
+	
+		playerManager.on("PlaybackInfoChanged", (status: Status) => {
+			console.log(status);
+		});
+	
+		playerManager.on("TimelinePropertiesChanged", (position: Position) => {
+			console.log(position);
+		});
+	}
 }
